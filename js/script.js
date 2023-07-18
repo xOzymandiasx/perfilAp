875fe673fd6fc3d18b50e1e8918aa4a96b55a3a0
Sergio Ottati <sergioottati@hotmail.com>
2023-07-18 11:14:51 -0300

    Más cambios al perfil!!!! barra de navegación con position stiky y botón para volver arriba en la página.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ observer.observe(document.querySelector(".miProfile"));
 observer.observe(document.querySelector(".cursos"));
 observer.observe(document.querySelector(".projects"));
 observer.observe(document.querySelector(".socials-div"));
+
+const buttonToTop = document.querySelector(".button-to-top");
+
+const myScrollFunc = () => {
+  const y = window.scrollY;
+  if (y >= 1200) {
+    buttonToTop.classList.add("tecno-visible");
+  } else {
+    buttonToTop.classList.remove("tecno-visible");
+  };
+};
+
+window.addEventListener("scroll", myScrollFunc);
